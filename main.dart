a21edc48195e8346aa81aa793b9c582bcbab9c61
hahrsrla <92637473+hahrsrla@users.noreply.github.com>
2021-10-16 21:11:37 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatApp extends StatelessWidget {
+
+ @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Chat App",
+      home: ChatScreen(),
+    );
+  }
+
+}
+  
